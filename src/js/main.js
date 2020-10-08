b5ca62af637fd0247b8aa3dd3d0d43d47f371cf6
xtel <evstafyevan96@gmail.com>
2020-10-08 12:34:45 +0500

    complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 headerScroll();
 document.querySelectorAll(".simplebar").forEach((el) => new SimpleBar(el));
-let mySwiper = new Swiper(".swiper-container", {
+let aboutUsSwiper = new Swiper(".about-us__swiper", {
   navigation: {
     nextEl: ".about-us__slider-btn--next",
     prevEl: ".about-us__slider-btn--prev",
     disabledClass: "about-us__slider-btn--disabled",
     hideOnClick: true,
   },
-  slidesPerView: "auto",
+  slidesPerView: 1,
 
   breakpoints: {
     576: {
@@ -15,6 +15,17 @@ let mySwiper = new Swiper(".swiper-container", {
       spaceBetween: 34,
     }
   }
+});
+let worksSwiper = new Swiper(".works__swiper", {
+  slidesPerView: 'auto',
+  grabCursor: true,
+  breakpoints: {
+    576: {
+      slidesPerView: 1,
+      centeredSlides: true
+    }
+  }
+
 });
 function headerScroll() {
   window.addEventListener("scroll", function () {
@@ -26,21 +37,3 @@ function headerScroll() {
     }
   });
 }
-let mobileToggle = document.querySelector(".header__nav-toggle");
-mobileToggle.addEventListener("click", function (evt) {
-
-  evt.preventDefault();
-  let header = document.querySelector(".header");
-  header.classList.toggle("header--isOpen");
-  let body = document.querySelector("body");
-  body.classList.toggle("lock");
-});
-let headerNavList = document.querySelector(".header__nav-list");
-headerNavList.addEventListener('click', function(evt) {
-  if (evt.target.closest('a')) {
-    let header = document.querySelector(".header");
-    header.classList.remove("header--isOpen");
-    let body = document.querySelector("body");
-    body.classList.remove("lock");
-  }
-});
